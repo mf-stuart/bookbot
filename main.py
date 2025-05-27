fdf56431e6caa27b1b4e362d364d182a17d23ed4
mf-stuart <MatthewZinkiewiczID@outlook.com>
2025-05-27 14:41:43 +0100

    added argument support

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from stats import get_book_length, get_char_dict, get_sorted_dict_list, print_char_count
+import sys
 
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
+def get_book_text(path):
+    with open(path) as f:
         file_content = f.read()
     return file_content
 
 def main():
+    try:
+        path = sys.argv[1]
+    except IndexError:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     print(
-"""
+f"""
 ============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt..."""
+Analyzing book found at {path}..."""
 )
 
-    text = get_book_text()
+    text = get_book_text(path)
     get_book_length(text)
     char_dict = get_char_dict(text)
     dict_list = get_sorted_dict_list(char_dict)
