81eedafe858ec3f33b613dfc4f8916443f8881db
mf-stuart <MatthewZinkiewiczID@outlook.com>
2025-05-27 13:12:33 +0100

    pre arguments implemented

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_book_length, get_char_dict
+from stats import get_book_length, get_char_dict, get_sorted_dict_list, print_char_count
 
 def get_book_text():
     with open("books/frankenstein.txt") as f:
@@ -6,9 +6,17 @@ def get_book_text():
     return file_content
 
 def main():
+    print(
+"""
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt..."""
+)
+
     text = get_book_text()
     get_book_length(text)
     char_dict = get_char_dict(text)
-    print(char_dict)
+    dict_list = get_sorted_dict_list(char_dict)
+    print_char_count(dict_list)
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,10 +1,26 @@
 def get_book_length(text):
+    print("----------- Word Count ----------")
     text_list = text.split()
     num_words = len(text_list)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
 def get_char_dict(text):
     char_dict = dict()
     for char in text:
         char_dict[char.lower()] = char_dict.get(char.lower(), 0) + 1
     return char_dict
+
+def get_sorted_dict_list(dict):
+    dict_list = []
+    for key, value in dict.items():
+        dict_list.append({"char": key, "num": value})
+    dict_list.sort(key=lambda x: x["num"], reverse=True)
+    return dict_list
+
+
+
+def print_char_count(dict_list):
+    print("--------- Character Count -------")
+    for entry in dict_list:
+        if entry['char'].isalpha():
+            print(f"{entry['char']}: {entry['num']}")
